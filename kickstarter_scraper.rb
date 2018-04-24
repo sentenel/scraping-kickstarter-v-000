34a638803354f7cb420aa202f9bd4b039f8fd554
Seth Wax <sethlwax@gmail.com>
2018-04-24 09:26:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+  binding.pry
 
   kickstarter.css("li.projects.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
@@ -24,7 +25,6 @@ def create_project_hash
     }
     binding.pry
   end
-  binding.pry
   projects
 end
 
