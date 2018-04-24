ea8a1e14a61da134be2b5181c501115542c8b864
Seth Wax <sethlwax@gmail.com>
2018-04-24 09:28:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,4 @@
 require 'nokogiri'
-require 'pry'
-
-#projects: kickstarter.css("li.project.grid_4")
-#title: project.css("h2.bbcard_name strong a").text
-#image link: project.css("div.project-thumbnail a img").attribute("src").value
-#description: project.css("p.bbcard_blurb").text
-#location: project.css("ul.project-mega span.location-name").text
-#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -23,7 +15,6 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
+  
   projects
 end
-
-create_project_hash
