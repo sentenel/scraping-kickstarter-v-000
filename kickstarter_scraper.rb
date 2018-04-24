5b0dc805b5ca3377254fe64ce527d94630845986
Seth Wax <sethlwax@gmail.com>
2018-04-24 09:10:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   #projects: kickstarter.css("li.projects.grid_4")
   #title: project.css("h2.bbcard_name strong a").text
   #image link: project.css("div.project-thumbnail a img").attribute("src").value
+  #description: projects.css("p.bbcard_blurb").text
 
 
   binding.pry
